39c53b9dc2ed8ee239c2d7e853d41796e450dc8e
Jordan Rosas <jordanrosas1203@gmail.com>
2018-12-05 14:29:22 -0600

    modularized the code for Daily Journal: have not started rewritting the functions yet.

---
+++ src/scripts/entriesDOM.js
@@ -0,0 +1,5 @@
+function appendingToDOM(entryHTML){
+    let container = document.querySelector('#container');
+    container.appendChild(entryHTML);
+}
+//this function appends the element to the DOM
